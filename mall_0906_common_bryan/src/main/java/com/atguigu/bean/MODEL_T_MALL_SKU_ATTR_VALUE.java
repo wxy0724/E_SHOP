0b11bd5839c679b379b1583aee40a5f59b197f28
Bryan <1348096958@qq.com>
2018-03-01 17:28:20 +0800

    beans

---
+++ mall_0906_common_bryan/src/main/java/com/atguigu/bean/MODEL_T_MALL_SKU_ATTR_VALUE.java
@@ -0,0 +1,17 @@
+package com.atguigu.bean;
+
+import java.util.List;
+
+public class MODEL_T_MALL_SKU_ATTR_VALUE {
+	private List<T_MALL_SKU_ATTR_VALUE> t_mall_sku_attr_val_list;
+
+	public List<T_MALL_SKU_ATTR_VALUE> getT_mall_sku_attr_val_list() {
+		return t_mall_sku_attr_val_list;
+	}
+
+	public void setT_mall_sku_attr_val_list(List<T_MALL_SKU_ATTR_VALUE> t_mall_sku_attr_val_list) {
+		this.t_mall_sku_attr_val_list = t_mall_sku_attr_val_list;
+	}
+
+	
+}
---
+++ mall_0906_common_bryan/src/main/java/com/atguigu/bean/OBJECT_T_MALL_ORDER.java
@@ -0,0 +1,19 @@
+package com.atguigu.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OBJECT_T_MALL_ORDER extends T_MALL_ORDER{
+	
+	List<OBJECT_T_MALL_FLOW> list_flow = new ArrayList<OBJECT_T_MALL_FLOW>();
+
+	public List<OBJECT_T_MALL_FLOW> getList_flow() {
+		return list_flow;
+	}
+
+	public void setList_flow(List<OBJECT_T_MALL_FLOW> list_flow) {
+		this.list_flow = list_flow;
+	}
+
+	
+}
